0398462fef8394459f9a33ec3d70df08bec75bac
danishaz <danishaz112345@gmail.com>
2023-06-23 00:52:54 +0300

    minor project clean

--- src/main/Utils.cpp
+++ src/main/Utils.cpp
@@ -14,8 +14,8 @@ Eigen::MatrixXcd qce::utils::kroneckerProduct(
     const std::size_t resultCols = first.cols() * second.cols();
 
     Eigen::MatrixXcd result(resultRows, resultCols);
-    for (unsigned i = 0; i < first.rows(); i++) {
-        for (unsigned j = 0; j < first.cols(); j++) {
+    for (Eigen::Index i = 0; i < first.rows(); i++) {
+        for (Eigen::Index j = 0; j < first.cols(); j++) {
             result.block(i*second.rows(), j*second.cols(), second.rows(), second.cols()) = first(i,j) * second;
         }
     }
@@ -47,8 +47,8 @@ qce::DynamicQubitState qce::utils::combineStates(
     std::size_t resultSize = first.size() * second.size();
     qce::DynamicQubitState result(resultSize);
     
-    for (std::size_t i = 0; i < first.size(); i++) {
-        for (std::size_t j = 0; j < second.size(); j++) {
+    for (Eigen::Index i = 0; i < first.size(); i++) {
+        for (Eigen::Index j = 0; j < second.size(); j++) {
             result[(i<<1)+j] = first[i] * second[j];
         }
     }
@@ -85,7 +85,7 @@ unsigned qce::utils::probabilityRandomChoice(Eigen::Matrix<double, -1, 1> probab
     double randTarget = (double)dist(engine);
 
     uint32_t sum = 0;
-    for (std::size_t i = 0; i < probabilities.size(); i++) {
+    for (Eigen::Index i = 0; i < probabilities.size(); i++) {
         double p = probabilities[i];
         double cur = sum + elementCount * p;
         if (sum <= randTarget && randTarget < cur) {
@@ -101,7 +101,7 @@ unsigned qce::utils::probabilityRandomChoice(Eigen::Matrix<double, -1, 1> probab
 Eigen::Matrix<double, -1, 1> qce::utils::convertAmplitudes2Probs(qce::DynamicQubitState qState) {
     Eigen::Matrix<double, -1, 1> probs(qState.size());
 
-    for (std::size_t i = 0; i < qState.size(); i++) {
+    for (Eigen::Index i = 0; i < qState.size(); i++) {
         Eigen::dcomplex ampl = qState[i];
         probs[i] = abs((ampl * ampl).real());
     }
